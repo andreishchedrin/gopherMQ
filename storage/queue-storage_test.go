864648c87067ed08ed8c8f336eb61e106c954abf
andreishchedrin <schedrin-post@rambler.ru>
2021-11-06 19:41:26 +0300

    add first test

---
+++ storage/queue-storage_test.go
@@ -0,0 +1,49 @@
+package storage
+
+import (
+	"andreishchedrin/gopherMQ/logger"
+	"github.com/golang-collections/collections/queue"
+	"testing"
+)
+
+func TestQueueStorage(t *testing.T) {
+	loggerInstance := &logger.Logger{}
+	storageInstance := QueueStorage{
+		Data:   make(map[string]*queue.Queue),
+		Logger: loggerInstance,
+	}
+
+	testChannel := "test_channel123"
+	testMessage := "test_message"
+	q := storageInstance.Set(Key{testChannel})
+	q.Enqueue(testMessage)
+
+	q, _ = storageInstance.Get(Key{testChannel})
+	message := q.Dequeue()
+
+	if message != testMessage {
+		t.Errorf("got %v, want %v", message, testMessage)
+	}
+
+	message = q.Dequeue()
+
+	if message != nil {
+		t.Errorf("got %v, want %v", message, nil)
+	}
+
+	result, _ := storageInstance.Delete(Key{testChannel})
+
+	if result != true {
+		t.Errorf("got %v, want %v", result, true)
+	}
+
+	q = storageInstance.Set(Key{testChannel})
+	q.Enqueue(testMessage)
+
+	storageInstance.Flush()
+	q, _ = storageInstance.Get(Key{testChannel})
+
+	if q != nil {
+		t.Errorf("got %v, want %v", q, nil)
+	}
+}
